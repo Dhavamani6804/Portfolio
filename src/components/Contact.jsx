5c80a5d8dc3bbfe7f2fb8ec13f192b78611b7b4b
Dhava <sdhavamani22@gmail.com>
2025-08-02 19:52:14 +0530

    improved some structure

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,24 +1,90 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const Contact = () => {
+  const [submitted, setSubmitted] = useState(false);
+const [formData, setFormData] = useState({
+  name: "",
+  email: "",
+  message: "",
+});
   return (
-    <div name='contact' className='scroll-mt-24 bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen'>
-      <div className='flex flex-col justify-center p-4 max-w-screen-lg mx-auto h-full'>
-        <div className='pb-8'>
-            <p className='text-4xl font-bold border-b-4 border-gray-500 p-2 inline'>Contact</p>
-            <p className='py-6'>Submit the form below to get in touch with me</p>
+    <div
+      name="contact"
+      className="scroll-mt-24 bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
+    >
+      <div className="flex flex-col justify-center p-4 max-w-screen-lg mx-auto h-full">
+        <div className="pb-8">
+          <p className="text-4xl font-bold border-b-4 border-gray-500 p-2 inline">
+            Contact
+          </p>
+          <p className="py-6">Submit the form below to get in touch with me</p>
         </div>
-        <div className='flex justify-center items-center'>
-            <form action="https://getform.io/f/bnllyomb" method="POST" className='flex flex-col w-full md:w-1/2'>
-                <input type="text" name='name' placeholder='Enter your name' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                <input type="text" name='email' placeholder='Enter your email' className='p-2 my-4 bg-transparent border-2 rounded-md text-white focus:outline-none' />
-                <textarea name="message" rows='10' placeholder='Enter your message' className='p-2 bg-transparent border-2 rounded-md text-white focus:outline-none'></textarea>
-                <button className='text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300'>Let's talk</button>
-            </form>
+        <div className="flex justify-center items-center">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+              const form = e.target;
+
+              fetch("https://getform.io/f/bnllyomb", {
+                method: "POST",
+                body: new FormData(form),
+              }).then(() => {
+                setSubmitted(true);
+                setFormData({ name: "", email: "", message: "" });
+              });
+            }}
+            className="flex flex-col w-full md:w-1/2"
+          >
+            <input
+              type="text"
+              name="name"
+              required
+              placeholder="Enter your name"
+              value={formData.name}
+              onChange={(e) =>
+                setFormData({ ...formData, name: e.target.value })
+              }
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <input
+              type="email"
+              name="email"
+              required
+              placeholder="Enter your email"
+              value={formData.email}
+              onChange={(e) =>
+                setFormData({ ...formData, email: e.target.value })
+              }
+              className="p-2 my-4 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            />
+            <textarea
+              name="message"
+              rows="10"
+              required
+              placeholder="Enter your message"
+              value={formData.message}
+              onChange={(e) =>
+                setFormData({ ...formData, message: e.target.value })
+              }
+              className="p-2 bg-transparent border-2 rounded-md text-white focus:outline-none"
+            ></textarea>
+
+            <button
+              type="submit"
+              className="text-white bg-gradient-to-b from-cyan-500 to-blue-500 px-6 py-3 my-8 mx-auto flex items-center rounded-md hover:scale-110 duration-300"
+            >
+              KIT...!
+            </button>
+          </form>
+          {submitted && (
+            <p className="text-green-500 mt-4">
+              Thank you for your message! I'll get back to you soon.
+            </p>
+          )}
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -8,17 +8,17 @@ const Portfolio = () => {
     {
       id: 1,
       src: reactWeather,
-      codeLink:"https://github.com/Dhavamani6804/weather-app.git"
+      codeLink: "https://github.com/Dhavamani6804/weather-app.git",
     },
     {
       id: 2,
       src: port,
-      codeLink:"https://github.com/Dhavamani6804/Portfolio.git"
+      codeLink: "https://github.com/Dhavamani6804/Portfolio.git",
     },
     {
       id: 3,
       src: chat,
-      codeLink:"https://github.com/Dhavamani6804/ConnectAll.git"
+      codeLink: "https://github.com/Dhavamani6804/ConnectAll.git",
     },
   ];
 
@@ -28,8 +28,8 @@ const Portfolio = () => {
       className="scroll-mt-24 bg-gradient-to-b from-black via-gray-900 to-gray-800 w-full text-white md:h-screen pt-20"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+        <div className="mt-16 sm:mt-8 md:mt-4 pb-8">
+          <p className=" text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
           <p className="py-6 text-gray-300">
@@ -38,20 +38,24 @@ const Portfolio = () => {
         </div>
 
         <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src,codeLink }) => (
+          {portfolios.map(({ id, src, codeLink }) => (
             <div
               key={id}
-className="shadow-lg shadow-cyan-700/30 rounded-lg bg-gray-900 hover:scale-105 hover:shadow-cyan-500/50 duration-500 transition-all ease-in-out"            >
+              className="shadow-lg shadow-cyan-700/30 rounded-lg bg-gray-900 hover:scale-105 hover:shadow-cyan-500/50 duration-500 transition-all ease-in-out"
+            >
               <div className="aspect-video overflow-hidden rounded-t-lg">
-  <img src={src} alt={`Project ${id}`} className="w-full h-full object-cover" />
-</div>
+                <img
+                  src={src}
+                  alt={`Project ${id}`}
+                  className="w-full h-full object-cover"
+                />
+              </div>
 
               <div className="flex items-center justify-center">
-                <a href={codeLink} target="_blank" rel="noreferrer"/>
+                <a href={codeLink} target="_blank" rel="noreferrer" />
                 <button className="w-full px-6 py-3 m-4 font-semibold text-cyan-400 bg-transparent border border-cyan-500 hover:text-white hover:bg-cyan-700 rounded-lg shadow-md transition duration-300 ease-in-out hover:shadow-cyan-500/40">
-  View Code
-</button>
-
+                  View Code
+                </button>
               </div>
             </div>
           ))}
