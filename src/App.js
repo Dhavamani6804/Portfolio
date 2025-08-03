20d2a48101ea3b7af42639a10bf0c56910ef8f51
Dhava <sdhavamani22@gmail.com>
2025-08-03 12:25:54 +0530

    removed unused imports

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import NavBar from "./components/NavBar";
 import Home from "./components/Home";
 import SocialLinks from "./components/SocialLinks";
 import About from "./components/About";
-import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
 import Skills from "./components/Skills";
 import Footer from "./components/Footer";
